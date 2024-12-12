8f0e30d3384ee2f26605fdb6253be046ae917797
Kevin Pannetier <pannetier.kevin@gmail.com>
2024-12-12 22:06:51 +0100

    add compression plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
+import autoprefixer from 'autoprefixer';
+import compression from 'vite-plugin-compression';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      // Enable both gzip and brotli
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+    compression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
